94c458b09f5e146c9973415c1ed6d947ca092a79
Jake Reilly <jakthorei@gmail.com>
2023-08-11 12:42:28 -0700

    tedt

--- golang_todd_random/main.go
+++ golang_todd_random/main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jathrei/puppy"
+)
 
 //comment to cehck git status
 // random note - gotta config email in order for git to show up on ghub
 
 func main() {
 	fmt.Println("Yooooo")
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1, s2)
 }
